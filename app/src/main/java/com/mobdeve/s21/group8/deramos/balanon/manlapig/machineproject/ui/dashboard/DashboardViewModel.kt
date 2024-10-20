e988a9ad54c4fda5092247a1ab22dba4613e62f1
Ghrazielle Rei <ghrazielle_deramos@dlsu.edu.ph>
2024-10-20 23:53:23 +0800

    Initial commit

---
+++ app/src/main/java/com/mobdeve/s21/group8/deramos/balanon/manlapig/machineproject/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.mobdeve.s21.group8.deramos.balanon.manlapig.machineproject.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/mobdeve/s21/group8/deramos/balanon/manlapig/machineproject/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.mobdeve.s21.group8.deramos.balanon.manlapig.machineproject.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
