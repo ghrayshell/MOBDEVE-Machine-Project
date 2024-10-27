f33f3e614c50a0f924db1df018c19909d162891a
Ghrazielle Rei <ghrazielle_deramos@dlsu.edu.ph>
2024-10-27 15:46:32 +0800

    Attach Catalogue Activity to NavBar

---
+++ app/src/main/java/com/mobdeve/s21/group8/deramos/balanon/manlapig/machineproject/ui/catalogue/CatalogueFragment.kt
@@ -0,0 +1,42 @@
+package com.mobdeve.s21.group8.deramos.balanon.manlapig.machineproject.ui.catalogue
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mobdeve.s21.group8.deramos.balanon.manlapig.machineproject.databinding.ActivityCatalogueBinding
+import com.mobdeve.s21.group8.deramos.balanon.manlapig.machineproject.ui.AdapterProductList
+
+class CatalogueFragment : Fragment() {
+
+    private var _binding: ActivityCatalogueBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val catalogueViewModel = ViewModelProvider(this).get(CatalogueViewModel::class.java)
+
+        _binding = ActivityCatalogueBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val recyclerView = binding.rvProductList
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val adapterProductList = AdapterProductList(requireContext(), catalogueViewModel.productModels)
+        recyclerView.adapter = adapterProductList
+
+        // Set up any additional UI elements as needed from the binding
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/mobdeve/s21/group8/deramos/balanon/manlapig/machineproject/ui/catalogue/CatalogueViewModel.kt
@@ -0,0 +1,23 @@
+package com.mobdeve.s21.group8.deramos.balanon.manlapig.machineproject.ui.catalogue
+
+import androidx.lifecycle.ViewModel
+import com.mobdeve.s21.group8.deramos.balanon.manlapig.machineproject.R
+import com.mobdeve.s21.group8.deramos.balanon.manlapig.machineproject.ui.ProductModel
+
+class CatalogueViewModel : ViewModel() {
+
+    val productModels = ArrayList<ProductModel>().apply {
+        val productImages = intArrayOf(R.drawable.blinds, R.drawable.blinds, R.drawable.blinds, R.drawable.blinds, R.drawable.blinds)
+        val productPricetag = intArrayOf(R.drawable.pricetag_white, R.drawable.pricetag_white, R.drawable.pricetag_white, R.drawable.pricetag_white, R.drawable.pricetag_white)
+        val productBookmarkBtns = intArrayOf(R.drawable.bookmark_checked, R.drawable.bookmark_white, R.drawable.bookmark_blue, R.drawable.bookmark_checked, R.drawable.bookmark_white)
+        val productAddBtns = intArrayOf(R.drawable.add_product, R.drawable.add_product, R.drawable.add_product, R.drawable.add_product, R.drawable.add_product)
+        val productNames = arrayOf("Combi Blinds", "Combi Blinds", "Combi Blinds", "Combi Blinds", "Combi Blinds")
+        val productFabrics = arrayOf("Fabric Type", "Fabric Type", "Fabric Type", "Fabric Type", "Fabric Type")
+        val productColors = arrayOf("Black, Red, White", "Black, Red, White", "Black, Red, White", "Black, Red, White", "Black, Red, White")
+        val productPrices = arrayOf("₱190/sqm.", "₱190/sqm.", "₱190/sqm.", "₱190/sqm.", "₱190/sqm.")
+
+        for (i in productNames.indices) {
+            add(ProductModel(productImages[i], productPricetag[i], productBookmarkBtns[i], productAddBtns[i], productNames[i], productFabrics[i], productColors[i], productPrices[i]))
+        }
+    }
+}
